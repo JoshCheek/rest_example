8773b5c3b4b6dae0b70381edd7435be3eadec099
Josh Cheek <josh.cheek@gmail.com>
2014-03-27 16:19:46 -0400

    Rails app with a scaffold for user (name, age)

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,6 @@
+json.extract! @user,
+  :id,
+  :name,
+  :age,
+  :created_at,
+  :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlayingAround::Application.config.secret_key_base = 'f8bab73e41b056f02d1d08bec088dcea6d4c2956f53814e0f1c6d55e1a85949c070491b190e66cdded8ab95572559c3d717d5f57495987ba82cd3b6a298569a3'
